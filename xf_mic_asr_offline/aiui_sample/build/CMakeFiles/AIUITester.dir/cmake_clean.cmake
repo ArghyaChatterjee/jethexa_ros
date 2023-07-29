e5bd3950d6993cf7373d3a801a6ba514151f3cf2
Arghya Chatterjee <arghyame20buet@gmail.com>
2023-07-29 06:26:27 +0600

    Add files via upload

---
+++ xf_mic_asr_offline/aiui_sample/build/CMakeFiles/AIUITester.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AIUITester.dir/src/AIUITester.cpp.o"
+  "CMakeFiles/AIUITester.dir/src/TestListener.cpp.o"
+  "CMakeFiles/AIUITester.dir/src/WriteAudioThread.cpp.o"
+  "CMakeFiles/AIUITester.dir/src/FileUtil.cpp.o"
+  "libAIUITester.pdb"
+  "libAIUITester.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AIUITester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ xf_mic_asr_offline/aiui_sample/build/CMakeFiles/aiui_sample.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nie/晓/R2.1.2/vvui/sample/aiui_sample/src/aiuiMain.cpp" "/home/nie/晓/R2.1.2/vvui/sample/aiui_sample/build/CMakeFiles/aiui_sample.dir/src/aiuiMain.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../../../include"
+  "../../../include/hid"
+  "/usr/include/json-c"
+  "/usr/lib/libusb/include/libusb-1.0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/nie/晓/R2.1.2/vvui/sample/aiui_sample/build/CMakeFiles/AIUITester.dir/DependInfo.cmake"
+  "/home/nie/晓/R2.1.2/vvui/sample/aiui_sample/build/CMakeFiles/audio_recorder.dir/DependInfo.cmake"
+  "/home/nie/晓/R2.1.2/vvui/sample/aiui_sample/build/CMakeFiles/audio_player.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
